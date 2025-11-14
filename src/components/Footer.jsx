455ba564dbb95589855be3b964dcc415cc2078e4
EseWey21 <144706509+EseWey21@users.noreply.github.com>
2025-11-14 10:18:03 -0600

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { MdEmail } from 'react-icons/md'
+import { MdEmail, MdLocationOn } from 'react-icons/md'
 import { FaWhatsapp, FaInstagram, FaFacebook } from 'react-icons/fa'
 
 function Footer() {
@@ -15,6 +15,15 @@ function Footer() {
             <MdEmail className="ecp-footer__icon" />
             <span>ecoprep_edu@hotmail.com</span>
           </a>
+          <a
+            href="https://www.google.com/maps/place/San+Francisco+49,+Los+Reyes,+56400+Los+Reyes+Acaquilpan,+Méx./@19.3586094,-98.9804265,17z/data=!3m1!4b1!4m6!3m5!1s0x85d1e29007ef259b:0x83430dd026943516!8m2!3d19.3586094!4d-98.9804265!16s%2Fg%2F11c1d6h2dr?entry=ttu&g_ep=EgoyMDI1MTExMS4wIKXMDSoASAFQAw%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ecp-footer__contact"
+          >
+            <MdLocationOn className="ecp-footer__icon" />
+            <span>San Francisco 49, Los Reyes, 56400 Los Reyes Acaquilpan, Méx.</span>
+          </a>
         </div>
 
         {/* Columna 2: WhatsApp */}
@@ -28,6 +37,10 @@ function Footer() {
             <FaWhatsapp className="ecp-footer__whatsapp-icon" />
             Escríbenos a WhatsApp
           </a>
+          <div className="ecp-footer__phone">
+            <p className="ecp-footer__phone-label">Para más dudas al:</p>
+            <p className="ecp-footer__phone-number">Teléfono: 56-5237-1464</p>
+          </div>
         </div>
 
         {/* Columna 3: Redes sociales */}
